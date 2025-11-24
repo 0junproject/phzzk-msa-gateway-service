3e75efbd117bff00033a95ab770751ac993ccab9
0yjunproject <0yjun.project@gmail.com>
2025-11-24 22:53:05 +0900

    gateway commit

---
+++ src/main/java/phzzk/phzzkmsagatewayservice/PhzzkMsaGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package phzzk.phzzkmsagatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhzzkMsaGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhzzkMsaGatewayServiceApplication.class, args);
+    }
+
+}
